106e426e2c2f836bbd405b1c2601e23ab7a75d05
toshinori.sato <tyahhahha@gmail.com>
2024-09-14 13:41:46 +0900

    try to use css-modules

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import styles from '@/app/ui/home.module.css'
 
 export default function Page() {
   return (
@@ -13,6 +14,7 @@ export default function Page() {
           <div
             className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
           />
+          <div className={styles.shape} />
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
